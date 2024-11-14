880859d338d5c5b6f60ed321951a76c585a02cd8
Mikhail <tsitsvero@gmail.com>
2024-11-14 12:02:10 +0900

    rendering molecules

--- templates/dummy_model/experiment.py
+++ templates/dummy_model/experiment.py
@@ -724,6 +724,94 @@ def train_dummy(dataset, out_dir, seed_offset):
     return final_info, train_log_info, val_log_info
 
 
+from ase.io import read
+from ase.visualize import view
+from ase import Atoms
+import matplotlib.pyplot as plt
+
+
+def render_molecule(xyz_path, out_path):
+    """Renders a molecule from an xyz file and saves it as a png image
+    
+    Args:
+        xyz_path (str): Path to input xyz file
+        out_path (str): Path to save output png file
+    """
+    # Read the xyz file
+    mol = read(xyz_path)
+    
+    # Create figure with no axes
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+    
+    # Plot the molecule
+    plot_atoms(mol, ax, rotation=('45x,0y,0z'), scale=0.5, radii=0.8)
+    
+    # Save figure
+    plt.savefig(out_path, dpi=300, bbox_inches='tight', transparent=True)
+    plt.close()
+
+
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+
+def render_molecule_rdkit(xyz_path, out_path):
+    """Renders a molecule from an xyz file using RDKit and saves it as a png image
+    
+    Args:
+        xyz_path (str): Path to input xyz file
+        out_path (str): Path to save output png file
+    """
+    # Read XYZ file
+    with open(xyz_path, "r") as f:
+        xyz_data = f.readlines()
+
+    # Parse number of atoms
+    num_atoms = int(xyz_data[0])
+
+    # Create RDKit molecule
+    mol = Chem.rdchem.RWMol()
+    conf = Chem.rdchem.Conformer(num_atoms)
+
+    # Add atoms and coordinates
+    for i in range(2, num_atoms+2):
+        line = xyz_data[i].split()
+        atom_symbol = line[0]
+        x = float(line[1])
+        y = float(line[2])
+        z = float(line[3])
+        
+        # Add atom
+        atom = Chem.rdchem.Atom(atom_symbol)
+        atom_idx = mol.AddAtom(atom)
+        
+        # Set 3D coordinates
+        conf.SetAtomPosition(atom_idx, (x, y, z))
+
+    # Add the conformer after all atoms are added
+    mol.AddConformer(conf)
+
+    # Connect atoms based on distance
+    for i in range(mol.GetNumAtoms()):
+        for j in range(i+1, mol.GetNumAtoms()):
+            pos_i = conf.GetAtomPosition(i)
+            pos_j = conf.GetAtomPosition(j)
+            dist = pos_i.Distance(pos_j)
+            
+            # Add bonds if atoms are close enough
+            if dist < 1.7:  # Typical bond length threshold in Angstroms
+                mol.AddBond(i, j, Chem.rdchem.BondType.SINGLE)
+
+    # Convert to regular molecule and render
+    mol = mol.GetMol()
+    img = Draw.MolToImage(mol)
+    img.save(out_path)
+
+
+
 
 if __name__ == "__main__":
     num_seeds = {
