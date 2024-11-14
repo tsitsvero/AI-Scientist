525722ac1d429d991ed011f801a772df6f87358d
Mikhail <tsitsvero@gmail.com>
2024-11-14 17:35:53 +0900

    reactdiff added

--- templates/dummy_model/run_scientist.sh
+++ templates/dummy_model/run_scientist.sh
@@ -4,4 +4,11 @@
 # gpt-4o-mini-2024-07-18
 
 
+pip install git+https://github.com/tsitsvero/OAReactDiff.git
+
+
+# python experiment.py --out-dir "./dummy_model_runs"
+python experiment.py --out_dir run_0
+python plot.py
+
 python launch_scientist.py --model "gpt-4o-mini-2024-07-18" --experiment dummy_model --num-ideas 1 --skip-novelty-check
