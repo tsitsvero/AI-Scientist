442a6cfa867dd4b9fa693b2450917005fbc74efb
Mikhail <tsitsvero@gmail.com>
2024-11-15 14:58:44 +0900

    run sci

--- templates/molecular_model/run_scientist.sh
+++ templates/molecular_model/run_scientist.sh
@@ -8,7 +8,5 @@ pip install git+https://github.com/tsitsvero/OAReactDiff.git
 
 
 # python experiment.py --out-dir "./dummy_model_runs"
-python experiment.py --out_dir run_0
-python plot.py
-
+python experiment.py --out_dir run_0; python plot.py
 python launch_scientist.py --model "gpt-4o-mini-2024-07-18" --experiment molecular_model --num-ideas 1 --skip-novelty-check > OUTPUT.log
